94909f8fac2e06360f32f24f863d4aaee22006d4
Huda Nassar <hnassar@purdue.edu>
2019-03-29 17:17:42 -0400

    updated pack and unpack functions

--- src/PackedMatrixMultiVectorProducts.jl
+++ src/PackedMatrixMultiVectorProducts.jl
@@ -1,7 +1,6 @@
 # PackedMatrixMultiVectorProducts
 
-# functions we will need:
-
+# pack and unpack functions we will need:
 function pack(X::AbstractArray{T,2}) where T<:Real
     # convert to Vector{SVector{size(k),T}}
     Xt = X'
@@ -13,24 +12,34 @@ function pack(X::AbstractArray{T,2}) where T<:Real
     return V
 end
 
-#unpack(x::Vector{T}) #
+function pack(X::Adjoint{T,AbstractArray{T,2}}) where T<:Real
+    # convert to Vector{SVector{size(k),T}}
+    Xt = X.parent
+    vector_size,nvectors = size(Xt)
+    V = Vector{SArray{Tuple{vector_size},T,1,vector_size}}(undef,nvectors)
+    for i = 1:nvectors
+        V[i] = SVector{vector_size}(Xt[:,i])
+    end
+    return V
+end
+
+# function unpack(V::Vector{T}) where T <: SArray
+#     k = length(V[1])
+#     n = length(V)
+#     X = zeros(n,k)
+#     for colid = 1:n
+#         X[colid,:] = Vector(V[colid])
+#     end
+#     return X
+# end
+
+# this method is faster and uses less allocations
 function unpack(V::Vector{T}) where T <: SArray
     k = length(V[1])
     n = length(V)
     X = zeros(n,k)
-    for colid = 1:n
-        X[colid,:] = Vector(V[colid])
+    for colid = 1:k
+        X[:,colid] = map(i->V[i][colid],1:n)
     end
     return X
 end
-
-# try a faster way
-# function unpack2(V::Vector{T}) where T <: SArray
-#     k = length(V[1])
-#     n = length(V)
-#     X = zeros(n,k)
-#     for colid = 1:k
-#         X[:,colid] = map(i->V[colid][i],1:n)
-#     end
-#     return X
-# end
