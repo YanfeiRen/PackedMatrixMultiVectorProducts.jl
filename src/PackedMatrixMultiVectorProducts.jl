343af8ee7acd951d1a83e6a398a251d98b5cf8fc
Huda Nassar <hnassar@purdue.edu>
2019-03-29 18:45:15 -0400

    Update PackedMatrixMultiVectorProducts.jl

--- src/PackedMatrixMultiVectorProducts.jl
+++ src/PackedMatrixMultiVectorProducts.jl
@@ -2,6 +2,10 @@
 module PackedMatrixMultiVectorProducts
 using LinearAlgebra
 using SparseArrays
+using StaticArrays
+using MatrixNetworks
+using BenchmarkTools
+
 include("pack_unpack.jl")
 
 export pack,unpack
