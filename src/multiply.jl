ef318a8523020a133890cd1e70f15bda5f46a060
YanfeiRen <ren105@purdue.edu>
2019-03-29 23:49:21 -0400

    Update multiply.jl
    Overload * operators for A'*x, A'*x', transpose(A)*x and transpose(A)*x'

--- src/multiply.jl
+++ src/multiply.jl
@@ -6,3 +6,11 @@ import Base.*
     (T = promote_type(TA, eltype(Tx)); LinearAlgebra.mul!(similar(x, similar_type(Tx, T, Size(Tx)), A.m), A, x, one(T), zero(T)))
 *(A::SparseMatrixCSC{TA,S}, B::StridedMatrix{Tx}) where {TA,S,Tx <: SArray} =
     (T = promote_type(TA, Tx); LinearAlgebra.mul!(similar(B, T, (A.m, size(B, 2))), A, B, one(T), zero(T)))
+*(adjA::Adjoint{<:Any,<:SparseMatrixCSC{TA,S}}, x::StridedVector{Tx}) where {TA,S,Tx <: SArray} =
+    (A = adjA.parent; T = promote_type(TA, eltype(Tx)); LinearAlgebra.mul!(similar(x, similar_type(Tx, T, Size(Tx)), A.n), adjoint(A), x, one(T), zero(T)))
+*(adjA::Adjoint{<:Any,<:SparseMatrixCSC{TA,S}}, B::StridedMatrix{Tx}) where {TA,S,Tx <: SArray} =
+    (A = adjA.parent; T = promote_type(TA, Tx); LinearAlgebra.mul!(similar(B, T, (A.n, size(B, 2))), adjoint(A), B, one(T), zero(T)))
+*(transA::Transpose{<:Any,<:SparseMatrixCSC{TA,S}}, x::StridedVector{Tx}) where {TA,S,Tx <: SArray} =
+    (A = transA.parent; T = promote_type(TA, eltype(Tx)); LinearAlgebra.mul!(similar(x, similar_type(Tx, T, Size(Tx)), A.n), transpose(A), x, one(T), zero(T)))
+*(transA::Transpose{<:Any,<:SparseMatrixCSC{TA,S}}, B::StridedMatrix{Tx}) where {TA,S,Tx <: SArray} =
+    (A = transA.parent; T = promote_type(TA, Tx); LinearAlgebra.mul!(similar(B, T, (A.n, size(B, 2))), transpose(A), B, one(T), zero(T)))
