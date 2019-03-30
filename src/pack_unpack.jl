7cdcc936ef04c106fbf8198ceb69820b5139e5cc
Huda Nassar <hnassar@purdue.edu>
2019-03-30 18:01:54 -0400

    n-by-0 vectors treatment

--- src/pack_unpack.jl
+++ src/pack_unpack.jl
@@ -1,37 +1,6 @@
-# TODO: we currently have an issue with X having a 0 dimension
-# SVectors do no support this: (possibly a bug in their code)
-#=
-julia> SVector{0}([])
-ERROR: UndefVarError: T not defined
-Stacktrace:
- [1] SArray{Tuple{0},T,1,0} where T(::Tuple{}) at /Users/hudanassar/.julia/packages/StaticArrays/VyRz3/src/SVector.jl:19
- [2] convert at /Users/hudanassar/.julia/packages/StaticArrays/VyRz3/src/convert.jl:26 [inlined]
- [3] SArray{Tuple{0},T,1,0} where T(::Array{Any,1}) at /Users/hudanassar/.julia/packages/StaticArrays/VyRz3/src/convert.jl:7
- [4] top-level scope at none:0
-
-julia> SVector{1}([0])
-1-element SArray{Tuple{1},Int64,1,1}:
- 0
-=#
-# But we should be able to support A*x when x is n-by-0
-#=
-julia> A = rand(n,k);
-
-julia> x = rand(k,0);
-
-julia> A*x
-10×0 Array{Float64,2}
-
-julia> @time A*x
-  0.000005 seconds (5 allocations: 240 bytes)
-10×0 Array{Float64,2}
-=#
-#= alternatively,
-We could check the dimensions at the beginning and treat them as special cases
-=#
-
 function pack(X::AbstractArray{T,2}) where T
     isbitstype(T) || error("$(T) is not a bitstype")
+    size(X,2)==0 && return repeat([SArray{Tuple{0},T,1,0}()],size(X,1))
     return reinterpret(SVector{size(X,2),T}, vec(copy(transpose(X))))
 end
 
