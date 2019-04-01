6bffbda9c1d1c9949e9e6e0f9859d1803ae749d8
David Gleich <dgleich@purdue.edu>
2019-03-31 21:31:14 -0400

    Small updates from David

--- test/mat_multi_vec_test.jl
+++ test/mat_multi_vec_test.jl
@@ -1,8 +1,24 @@
+using SparseArrays
+using LinearAlgebra
 @testset "mat_multi_vec" begin
     A = sprand(10_000,10_000,1/10_000)
     X = randn(10_000,32)
     @test A*X ≈ unpack(A*pack(X))
-     
+
     X = randn(32,10_000)
     @test A*X' ≈ unpack(A*pack(X'))
+end
+
+@testset "operation_tests" begin
+    X = randn(10,2)
+    @test (5I)*X == unpack((5I)*pack(X))
+
+    D = Diagonal(randn(10))
+    @test (D)*X == unpack((D)*pack(X))
+
+    T = Tridiagonal(randn(10,10))
+    @test (T)*X == unpack((T)*pack(X))
+
+    #L = LowerTriangular(ones(10,10))
+    #@test (L)*X == unpack((L)*pack(X))
 end
