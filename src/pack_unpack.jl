adb5f51f45f07dc3bf1a4b726bbf25658a4b6b7b
Huda Nassar <hnassar@purdue.edu>
2019-04-02 16:11:08 -0400

    added a faster unpack function which preallocates memory

--- src/pack_unpack.jl
+++ src/pack_unpack.jl
@@ -6,8 +6,19 @@ end
 
 # based on
 # https://github.com/JuliaArrays/StaticArrays.jl/pull/496/commits/92287a299af7bfc47cf3b26c135c6ce6a4c99ba0
-function unpack(a::StridedVector{SArray{SZT, T, NDIMS, L}}) where {T,SZT,NDIMS,L}
-    isbitstype(T) || error("$(T) is not a bitstype")
-    szres = (size(eltype(a))..., size(a)...)
-    return copy(transpose(reshape(reinterpret(T, a),szres)))
+# function unpack(a::StridedVector{SArray{SZT, T, NDIMS, L}}) where {T,SZT,NDIMS,L}
+#     isbitstype(T) || error("$(T) is not a bitstype")
+#     szres = (size(eltype(a))..., size(a)...)
+#     return copy(transpose(reshape(reinterpret(T, a),szres)))
+# end
+
+
+function new_unpack(a::StridedVector{SArray{SZT, T, NDIMS, L}}) where {T,SZT,NDIMS,L}
+    k = L
+    n = length(a)
+    X = Matrix{T}(undef, n,k)
+    @inbounds for rowid = 1:n
+        X[rowid,:] .= a[rowid]
+    end
+    return X
 end
