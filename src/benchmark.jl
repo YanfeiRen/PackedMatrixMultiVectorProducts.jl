2d7249a179772981dcdcc9d497518295d9041aa5
Huda Nassar <hnassar@purdue.edu>
2019-04-04 01:50:00 -0400

    reformatting of benchmark.jl

--- src/benchmark.jl
+++ src/benchmark.jl
@@ -1,18 +1,28 @@
 function benchmark(A,X)
     @assert A*X ≈ unpack(A*pack(X))
-    println("###################")
-    println("typeof(A) = $(typeof(A))")
-    println("typeof(X) = $(typeof(X))")
-    println("size(A) = $(size(A))")
-    println("size(X) = $(size(X))")
-    println("Benchmark Julia's matmul")
-    b = @benchmark $A*$X
-    display(b)
-    println("")
-    println("Benchmark PackedMatrixMultiVectorProducts")
-    b = @benchmark unpack($A*pack($X))
-    display(b)
-    println("###################")
+    
+    b1 = @benchmark mul!(Y,$A,$X) setup=(Y=zeros(size($A,1),size($X,2)))
+    
+    pX = pack(X)
+    b2 = @benchmark mul!(Y,$A,$pX) setup=(Y=zeros(SVector{size($X,2),Float64},size($A,1)))
+               
+    b3 = @benchmark unpack($A*pack($X))
+    
+    m1 = median(b1.times)
+    m2 = median(b2.times)
+    m3 = median(b3.times)
+    r1 = m1/m2
+    r2 = m1/m3
+    s1 = BenchmarkTools.prettytime(m1)
+    s2 = BenchmarkTools.prettytime(m2)
+    s3 = BenchmarkTools.prettytime(m3)
+    
+    typeof(X) <: Adjoint ? sizex = size(X.parent) : sizex = size(X)
+    @printf("(%d,%d)\t(%d,%d)\t\t%s\t%s\t\t%s\t%.2f\t%.2f\n",
+            size(A)...,
+            sizex...,
+            s1, s2, s3,
+            r1,r2)
 end
 
 function benchmark(n::Int64,p::Float64,k::Int64)
@@ -26,38 +36,32 @@ function benchmark(n::Int64,p::Float64,k::Int64)
     benchmark(At,Xt)
 end
 
-function benchmark()
+function mybenchmark(n::Int64)
     kvalues = [2,4,8,16]
-    n = 50_000
-    p = 10/50_000
+    @assert n>=100 #just to get a reasonable size network
+    p = 10/n
     A = sprand(n,n,p)
     At = A'
     
-    println("##### Benchmarks for A*X #####")
-    for k in kvalues
-        X = randn(n,k)
-        println("***** k = $k *****")
-        benchmark(A,X)
-    end
+    @printf("size(A)\t\tsize(x)\t\tmul!_time\tMV_time\t\tPMV_time\tMV_speedup\tPMV_speedup\n")   
     
-    println("##### Benchmarks for A*X' #####")
+#     for k in kvalues
+#         X = randn(n,k)
+#         benchmark(A,X)
+#     end
+    
     for k in kvalues
         Xt = randn(k,n)'
-        println("***** k = $k *****")
         benchmark(A,Xt)
     end
     
-    println("##### Benchmarks for A'*X #####")
     for k in kvalues
         X = randn(n,k)
-        println("***** k = $k *****")
         benchmark(At,X)
     end
     
-    println("##### Benchmarks for A'*X' #####")
     for k in kvalues
         Xt = randn(k,n)'
-        println("***** k = $k *****")
         benchmark(At,Xt)
     end
 end
