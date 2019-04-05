208ecb4202298bde3029747b8fde68eae0c09810
Huda Nassar <hnassar@purdue.edu>
2019-04-05 19:44:04 -0400

    new benchmarks

--- src/benchmark.jl
+++ src/benchmark.jl
@@ -1,41 +1,47 @@
 using Printf
 using Statistics
+using Markdown
 function benchmark(A,X)
     @assert A*X ≈ unpack(A*pack(X))
     
-    b1 = @benchmark mul!(Y,$A,$X) setup=(Y=zeros(size($A,1),size($X,2)))
+    # benchmark mul!(Y,A,X)
+    Y = zeros(size(A,1),size(X,2))
+    b1 = @benchmark mul!($Y,$A,$X)
     
+    # benchmark mul!(y,A,x)
     pX = pack(X)
-    b2 = @benchmark mul!(Y,$A,$pX) setup=(Y=zeros(SVector{size($X,2),Float64},size($A,1)))
-               
-    b3 = @benchmark unpack($A*pack($X))
+    y = zeros(SVector{size(X,2),Float64},size(A,1))
+    b2 = @benchmark mul!($y,$A,$pX) 
     
+    # benchmark A*X
+    b3 = @benchmark $A*$X
+    
+    # benchmark unpack(A*pack(X))
+    b4 = @benchmark unpack($A*pack($X))
+    
+    # numeric times
     m1 = median(b1.times)
     m2 = median(b2.times)
     m3 = median(b3.times)
-    r1 = m1/m2
-    r2 = m1/m3
+    m4 = median(b4.times)
+    
+    # nice looking times (in strings)
     s1 = BenchmarkTools.prettytime(m1)
     s2 = BenchmarkTools.prettytime(m2)
     s3 = BenchmarkTools.prettytime(m3)
+    s4 = BenchmarkTools.prettytime(m4)
     
-    typeof(X) <: Adjoint ? sizex = size(X.parent) : sizex = size(X)
-    @printf("(%d,%d)\t(%d,%d)\t\t%s\t%s\t%s\t%.2f\t\t%.2f\n",
-            size(A)...,
-            sizex...,
-            s1, s2, s3,
-            r1,r2)
-end
+    # speedups:
+    # mul!(Y,A,X) vs mul!(y,A,x)
+    r1 = m1/m2
+    
+    # A*X vs unpack(A*pack(X))
+    r2 = m3/m4
 
-function benchmark(n::Int64,p::Float64,k::Int64)
-    A = sprand(n,n,p)
-    X = randn(n,k)
-    Xt = randn(k,n)'
-    At = A'
-    benchmark(A,X)
-    benchmark(A,Xt)
-    benchmark(At,X)
-    benchmark(At,Xt)
+    # return array of all info needed
+    typeof(X) <: Adjoint ? sizex = size(X.parent) : sizex = size(X)    
+    return Any[size(A),sizex, s1,s2,round(r1,digits=2),
+                              s3,s4,round(r2,digits=2)]
 end
 
 function benchmark(n::Int64)
@@ -44,26 +50,32 @@ function benchmark(n::Int64)
     p = 10/n
     A = sprand(n,n,p)
     At = A'
-    
-    @printf("size(A)\t\tsize(x)\t\tmul!_time\tMV_time\t\tPMV_time\tMV_speedup\tPMV_speedup\n")   
+    rows = []
+    push!(rows,Any["size(A)","size(x)","mul!(Y,A,X)","mul!(y,A,x)","Ratio",
+                                        "Julia's A*X","unpack(A*pack(X))","Ratio"])
+
+    for k in kvalues
+        X = randn(n,k)
+        b = benchmark(A,X)
+        push!(rows,b)
+    end
+    for k in kvalues
+        Xt = randn(k,n)'
+        b = benchmark(A,Xt)
+        push!(rows,b)
+    end
     
     for k in kvalues
         X = randn(n,k)
-        benchmark(A,X)
+        b = benchmark(At,X)
+        push!(rows,b)
     end
     
     for k in kvalues
         Xt = randn(k,n)'
-        benchmark(A,Xt)
+        b = benchmark(At,Xt)
+        push!(rows,b)
     end
-    
-    for k in kvalues
-        X = randn(n,k)
-        benchmark(At,X)
-    end
-    
-    for k in kvalues
-        Xt = randn(k,n)'
-        benchmark(At,Xt)
-    end
+    table = Markdown.MD(Any[Markdown.Table(rows, Symbol[:l, :r, :l, :r,:r,:r,:r,:r])])
+    println(table)
 end
